d91e50d913d4ed4df0a0a58052b2f7c41e0460da
zq <285087573@qq.com>
2021-07-22 15:58:37 +0800

    7-22

--- configclient/src/main/java/com/m/Application.java
+++ configclient/src/main/java/com/m/Application.java
@@ -2,8 +2,9 @@ package com.m;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
